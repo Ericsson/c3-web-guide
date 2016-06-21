8542c2e0fb1f3014c8ef4ab6ee7f4fbac2269392
Gustav Fridell <gustavfridell@me.com>
2016-06-21 13:44:45 +0200

    add basic styling and Codearea component

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,10 @@ var config = {
                 query: {
                     presets: ['es2015', 'react']
                 }
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader"
             }
         ]
     }
