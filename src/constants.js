55126ea05960e92d9b814ef0d0090780747a41dd
Gustav Fridell <gustavfridell@me.com>
2016-07-13 13:10:47 +0200

    lighten default text color (#18)

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const serverUrl = 'https://webrtc-test.cct.ericsson.net';
 export const pageTitle = 'C3 Web Guide';
-export const defaultTextColor = '#333';
+export const defaultTextColor = '#444';
 export const defaultBorderColor = '#e1e1e1';
 export const defaultBorder = `1px solid ${defaultBorderColor}`;
