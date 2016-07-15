f8525a7e19b77b998add4b101c810a8b53b787f8
Gustav Fridell <gustavfridell@me.com>
2016-07-15 11:48:10 +0200

    improve authentication page

--- src/pages/authentication.js
+++ src/pages/authentication.js
@@ -1,11 +1,21 @@
+// Registering and authenticating a user:
+cct.Auth.registerWithPassword({
+    username: 'exampleName',
+    password: 'examplePassword',
+    serverUrl: 'https://example.domain'
+})
+.then(client.auth)
+.then(function(client) {
+    console.log('Client registered and authenticated with user id: ' + client.user.id);
+})
+
+// Logging in and authenticating an existing user:
 cct.Auth.loginWithPassword({
-        username: 'foo',
-        password: 'password123',
-        serverUrl: 'https://example.com'
-    })
-    .then(client.auth) // client methods are bound to the client
-    .then(function (client) {
-        console.log('Authenticated with user id: ' + client.user.id);
-    }, function (error) {
-        console.error('Failed to authenticate user: ' + error);
-    });
+    username: 'exampleName',
+    password: 'examplePassword',
+    serverUrl: 'https://example.domain'
+})
+.then(client.auth)
+.then(function (client) {
+    console.log('Client logged in and authenticated with user id: ' + client.user.id);
+});
--- src/pages/authentication.md
+++ src/pages/authentication.md
@@ -1,2 +1,4 @@
 ## Authentication
-A client is authenticated using an AuthInfo object, which can be acquired in different ways. The Auth class contains some static methods with common authentication methods. Let's start by logging on to a user that was previously registered using `Auth.registerWithPassword`.
+A client is authenticated using an AuthInfo object, which can be acquired in different ways. The Auth class contains some static methods with common authentication methods.
+
+The code on the right showcases how to register a user and how to log in an already existing user.
